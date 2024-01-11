e83fdd774122cca55e3cd471e0e54534636bbe7b
TranThienPhuc2657 <145333256+TranThienPhuc2657@users.noreply.github.com>
2024-01-11 13:50:01 +0700

    NUMMAX.cpp in 090124.pdf

---
+++ NUMMAX.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define file(name) if (fopen(name".inp","r")) {freopen(name".inp","r",stdin); freopen(name".out","w",stdout);}
+#define pb push_back
+#define fi first
+#define se second
+#define REP(i,n) for (int i=0; i<n; i++)
+#define REPD(i,n) for (int i=n-1; i>=0; i--)
+#define FOR(i,a,b) for (int i=a; i<=b; i++)
+#define FORD(i,a,b) for (int i=a; i>=b; i--)
+#define ll long long
+#define vi vector <int>
+#define vll vector <ll>
+#define pii pair <int,int>
+#define INF LLONG_MAX
+const int N=1e6+5;
+
+int n,k,a[N],l,r,z=0,y;
+ll res=0;
+bool check[N];
+vi b;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    file("NUMMAX");
+
+    cin >> n >> k;
+    REP(i,n) {cin >> a[i]; if (a[i]==k) b.pb(i);}
+    a[n]=INT_MAX;
+    REP(j,b.size()) {
+        if (!check[b[j]]) {
+            check[b[j]]=true;
+            z=b[j];
+            int l=0; int r=0;
+            FORD(i,b[j]-1,0) {
+                if (a[i]>k) break;
+                l++;
+            }
+            FOR(i,b[j],n) {
+                if (a[i]>k) {y=i; break;}
+                if (a[i]==k) check[i]=true;
+                r++;
+            }
+            res+=r*(l+1);
+        }
+        else {res+=(y-b[j])*(b[j]-z); z=b[j];}
+    }
+    cout << res;
+    return 0;
+}
